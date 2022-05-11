d158cc2cca05b31c50bcde0542d428729d5c5089
haryu <ryu.axel@gmail.com>
2022-05-11 22:51:25 +0900

    study and make example codes for functs.

---
+++ haryu/ex_readline_series.c
@@ -0,0 +1,41 @@
+#include <signal.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void handler(int signum)
+{
+    if (signum != SIGINT)
+        return;
+    printf("ctrl + c\n");
+    rl_on_new_line();
+    rl_replace_line("", 1);
+    rl_redisplay();
+}
+
+int main(void)
+{
+    int ret;
+    char *line;
+
+    signal(SIGINT, handler);
+    while (1)
+    {
+        line = readline("Jarvis ►>/ ");
+        if (line)
+        {
+            if (ret)
+                printf("Jarvis : You typed [%s]\n", line);
+            add_history(line);
+            free(line);
+            line = NULL;
+        }
+        else
+        {
+            printf("ctrl + d\n");
+        }
+    }
+    return (0);
+}
