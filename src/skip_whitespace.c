6d5ca055d29b187914463e68029aebf6f1204455
Cgim <cksrb1526@naver.com>
2022-06-24 10:08:02 +0900

    fix: skip_whitespace

--- src/skip_whitespace.c
+++ src/skip_whitespace.c
@@ -6,15 +6,23 @@
 /*   By: cgim <cgim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 11:27:34 by cgim              #+#    #+#             */
-/*   Updated: 2022/06/10 11:27:35 by cgim             ###   ########.fr       */
+/*   Updated: 2022/06/24 10:01:49 by cgim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	is_whitespace(char chr)
+{
+	if (chr == '\t' || chr == '\n' || chr == '\v'
+		|| chr == '\f' || chr == '\r' || chr == ' ')
+		return (1);
+	return (0);
+}
+
 int	skip_whitespace(char *cmd, int i)
 {
-	while (cmd[i] == ' ')
+	while (is_whitespace(cmd[i]))
 		i++;
 	return (i);
 }
