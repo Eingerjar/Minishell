612471b6fc1d83b39a57eaa2cb8baefae488c4d2
haryu <ryu.axel@gmail.com>
2022-06-30 13:29:46 +0900

    UPDATE 1. FIX : whitespace errors(parsing part) 2. ADD : whitespace line parsing conditional part(in pre_error_check    part).

--- src/error_skip_whitespace.c
+++ src/error_skip_whitespace.c
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 01:46:17 by haryu             #+#    #+#             */
-/*   Updated: 2022/06/23 21:45:39 by haryu            ###   ########.fr       */
+/*   Updated: 2022/06/30 13:25:18 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	skip_whitespace_r(int i, char *line)
 {
-	while (line[i] == 32)
+	while (is_whitespace(line[i]))
 	{
 		if (i >= (int)ft_strlen(line))
 			return (PARSE);
-		else if (line[i] != 32)
+		else if (is_whitespace(line[i]))
 			break ;
 		i++;
 	}
