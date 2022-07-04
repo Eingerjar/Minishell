4ad15fb914120c4aef0a200cad686d159934be81
haryu <ryu.axel@gmail.com>
2022-07-04 17:44:54 +0900

    UPDATE FIX : heredoc error handling (cat << "" cat << "    ")

--- src/here_make_flist.c
+++ src/here_make_flist.c
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 21:57:15 by haryu             #+#    #+#             */
-/*   Updated: 2022/07/01 21:31:34 by haryu            ###   ########.fr       */
+/*   Updated: 2022/07/04 17:42:32 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,42 @@ static char	*ft_strndup(char *line, size_t byte)
 	return (ret);
 }
 
-void	make_flist(int start, int end, char *line, t_flist **target)
+static void	ft_flist_add_back(t_flist **target, t_flist **temp)
 {
-	t_flist	*temp;
 	t_flist	*copy;
 
 	copy = (*target);
-	temp = malloc_wrap(sizeof(t_flist) * 1);
-	temp->next = 0;
-	temp->type = 1;
-	if (line[start] == '"' || line[start] == '\'')
-	{
-		end = skip_quotes(line, start, line[start]) - 1;
-		start++;
-	}
-	temp->name = ft_strndup(line + start, (size_t)(end - start));
 	while (TRUE)
 	{
 		if (copy->next == 0)
 			break ;
 		copy = copy->next;
 	}
-	copy->next = temp;
+	copy->next = (*temp);
+	return ;
+}
+
+void	make_flist(int start, int end, char *line, t_flist **target)
+{
+	t_flist	*temp;
+	int		special;
+
+	temp = malloc_wrap(sizeof(t_flist) * 1);
+	temp->next = 0;
+	temp->type = 1;
+	special = 0;
+	if (line[start] == '"' || line[start] == '\'')
+	{
+		end = skip_quotes(line, start, line[start]);
+		start++;
+		if (start == end)
+		{
+			temp->name = ft_strndup("", 1);
+			special = 1;
+		}
+	}
+	if (special == 0)
+		temp->name = ft_strndup(line + start, (size_t)(end - start));
+	ft_flist_add_back(target, &temp);
 	return ;
 }
