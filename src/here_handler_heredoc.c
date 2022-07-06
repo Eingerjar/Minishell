df7be5dd20407ca937d812e2b16d6ea3868cdf48
haryu <ryu.axel@gmail.com>
2022-07-07 04:03:35 +0900

    Delete : delte sigquit handling conditions

--- src/here_handler_heredoc.c
+++ src/here_handler_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 12:08:21 by haryu             #+#    #+#             */
-/*   Updated: 2022/07/07 03:24:58 by haryu            ###   ########.fr       */
+/*   Updated: 2022/07/07 04:02:19 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 void	handler_heredoc(int signum)
 {
-	if (signum == SIGQUIT)
-	{
-		return ;
-	}
 	exit(1);
 }
