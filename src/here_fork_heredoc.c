78aefb4491a5de18bfef2809ea5a0f538d3287f8
haryu <ryu.axel@gmail.com>
2022-07-06 19:49:01 +0900

    fix : heredoc global exit code conversion fixed

--- src/here_fork_heredoc.c
+++ src/here_fork_heredoc.c
@@ -6,13 +6,13 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 16:43:02 by haryu             #+#    #+#             */
-/*   Updated: 2022/07/01 04:47:17 by haryu            ###   ########.fr       */
+/*   Updated: 2022/07/06 19:40:12 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	fork_heredoc(t_flist **heredoc, int height, char *installed)
+void	fork_heredoc(t_flist **heredoc, int height, char *installed, int prev)
 {
 	pid_t	here_child;
 	int		status;
@@ -22,7 +22,7 @@ void	fork_heredoc(t_flist **heredoc, int height, char *installed)
 	if (here_child == -1)
 		printf("%sfork error\n", RED);
 	if (here_child == 0)
-		child_heredoc(heredoc, height, installed);
+		child_heredoc(heredoc, height, installed, prev);
 	else
 		wait(&status);
 	g_global.last_exitcode = WEXITSTATUS(status);
