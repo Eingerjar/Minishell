3134f8701f794b846a353af7c599546f4e940bef
haryu <ryu.axel@gmail.com>
2022-07-07 17:44:55 +0900

    FIX : echo & signal handler 1. FIX : echo argv option logic 2. FIX : on main prompt, if we use signal, handler will add 1 value to    global var.

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 02:41:19 by haryu             #+#    #+#             */
-/*   Updated: 2022/07/06 18:46:59 by haryu            ###   ########.fr       */
+/*   Updated: 2022/07/07 17:41:37 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,28 +54,55 @@ static int	check_flag(int max, char **argv)
 		return (FALSE);
 }
 
+static int	check_is_flag(char *str, int *flag, int *other, int first)
+{
+	int	i;
+
+	i = 1;
+	if (*other == TRUE)
+		return (FALSE);
+	if (str[0] != '-')
+	{
+		*other = TRUE;
+		return (FALSE);
+	}
+	while (str[i])
+	{
+		if (str[i] != 'n')
+		{
+			*other = TRUE;
+			return (FALSE);
+		}
+		i++;
+	}
+	(*flag) = 1;
+	if (!first)
+		return (FALSE);
+	return (TRUE);
+}
+
 void	builtin_echo(char **argv)
 {
 	int	i;
 	int	max;
+	int	other;
 	int	flag;
+	int	first;
 
 	i = 0;
 	max = echo_argv_length(argv);
 	flag = check_flag(max, argv);
 	if ((max == 1 || (max == 2 && flag == TRUE)) && no_argument(flag))
 		return ;
+	init_echo_flags(&flag, &first, &other);
 	while (++i < max)
 	{
-		if (i == 1 && flag == TRUE)
+		if (check_is_flag(argv[i], &flag, &other, first))
 			continue ;
-		write(1, argv[i], ft_strlen(argv[i]));
-		if (i + 1 == max)
-		{
-			if (flag == FALSE)
-				write(1, "\n", 1);
+		else
+			write(1, argv[i], ft_strlen(argv[i]));
+		if (i + 1 == max && echo_print_nl(flag))
 			break ;
-		}
 		write(1, " ", 1);
 	}
 	g_global.last_exitcode = 0;
